384e1a40d50d54bed0b95b867a850c3d1e88ad41
Heliton Cantalice <helitoncantalice.dev@gmail.com>
2025-09-17 09:29:16 -0300

    Create ex1.py
    Exercícios de estrutura de decisão

---
+++ Python/2 - Estrutura de decisão/ex1.py
@@ -0,0 +1,9 @@
+#Faça um programa que peça dois números e imprima o maior deles.
+
+n1 = int(input('Digite o n1: '))
+n2 = int(input('Digite o n2: '))
+
+if n1 > n2 :
+    print(n1)
+else:
+    print(n2)
