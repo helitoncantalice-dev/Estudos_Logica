bb3dfcfd3cb25d3b445236e3af14d5ac9d05f2c7
Heliton Cantalice <helitoncantalice.dev@gmail.com>
2025-09-18 10:26:21 -0300

    Create ex17.py

---
+++ Python/2 - Estrutura de decisão/ex17.py
@@ -0,0 +1,9 @@
+#Faça um programa que peça um número correspondente a um 
+#determinado ano e em seguida informe se este ano é ou não bissexto.
+
+ano = int(input('Digite um ano '))
+situacao = None
+if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+    print('bissexto')
+else:
+    print('não bissexto')
